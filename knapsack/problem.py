68874ff7d9a9ee64a48916dc55169f3c6f8d9b91
ali <ali.sdfi.dev@gmail.com>
2022-07-17 09:10:24 +0430

    update: fix get_input

--- knapsack/problem.py
+++ knapsack/problem.py
@@ -23,7 +23,7 @@ def get_input(n : int) -> tuple:
     weights = []
     values = []
     for i in range(1, n+1):
-        w, v = map(int, lines[i].split())
+        v, w = map(int, lines[i].split())
         values.append(v)
         weights.append(w)
         
